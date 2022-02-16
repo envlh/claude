8800f43c70c9f267158ddc2796e733feeab73dc3
Envel Le Hir <envel@lehir.net>
2022-02-16 13:08:10 +0100

    database: convert to utf8mb4_cs_0900_as_cs

--- sql/db.sql
+++ sql/db.sql
@@ -1,20 +1,20 @@
 CREATE TABLE `crawl` (
   `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `url` varchar(255) NOT NULL,
+  `url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_cs_0900_as_cs NOT NULL,
   `date` datetime NOT NULL,
   `status_code` smallint unsigned NOT NULL,
-  `headers` mediumtext NOT NULL,
-  `content` mediumtext NOT NULL,
+  `headers` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_cs_0900_as_cs NOT NULL,
+  `content` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_cs_0900_as_cs NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `url_date_UNIQUE` (`url`,`date`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_as_cs;
 
 CREATE TABLE `history` (
   `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `entity` varchar(16) NOT NULL,
-  `property` varchar(16) NOT NULL,
-  `value` varchar(128) NOT NULL,
+  `entity` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_cs_0900_as_cs NOT NULL,
+  `property` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_cs_0900_as_cs NOT NULL,
+  `value` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_cs_0900_as_cs NOT NULL,
   `date` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `entity_property` (`entity`,`property`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_as_cs;
