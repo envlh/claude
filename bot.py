a982e9658d9f8224e32455df86c7825629a092dc
Envel Le Hir <envel@lehir.net>
2022-07-29 11:18:27 +0200

    main: processing all dictionaries at once

--- bot.py
+++ bot.py
@@ -32,42 +32,44 @@ def main():
     site = get_site()
     configuration = utils.load_json_file('conf/general.json')
     db = DB(configuration['database'])
-    dico = LeRobert(db)
-    lexemes = dico.fetch_lexemes_to_crawl()
-    success_count = 0
-    errors = dict()
-    ref = dict()
-    for lexeme in lexemes:
-        lexeme_id = lexeme['lexeme']['value'][31:]
-        if not db.exists_history(lexeme_id, dico.get_property_id()):
-            success, inferred_id = dico.process(lexeme)
-            if success:
-                add_value(site, db, lexeme_id, dico.get_property_id(), inferred_id, dico.get_edit_summary())
-                success_count += 1
-            else:
-                errors[lexeme_id] = inferred_id
-                ref[lexeme_id] = lexeme
-    errors = dict(sorted(errors.items(), key=lambda item: item[1]))
-    errors_string = '{| class="wikitable sortable"\n!Lexeme\n!Lemma\n!Lexical category\n!Grammatical gender\n!Inferred ID\n'
-    for lexeme_id in errors:
-        errors_string += '|-\n'
-        errors_string += '| [[Lexeme:' + lexeme_id + '|' + lexeme_id + ']]\n'
-        errors_string += '| ' + ref[lexeme_id]['lemma']['value'] + '\n'
-        errors_string += '| ' + ref[lexeme_id]['lexicalCategoryLabel']['value'] + '\n'
-        errors_string += '| '
-        if 'genderLabel' in ref[lexeme_id]:
-            errors_string += ref[lexeme_id]['genderLabel']['value']
-        errors_string += '\n'
-        errors_string += '| '
-        if errors[lexeme_id] != '':
-            errors_string += '[' + dico.get_url().format(errors[lexeme_id]) + ' ' + errors[lexeme_id] + ']'
-        errors_string += '\n'
-    errors_string += '|}'
-    report_page = pywikibot.Page(site, 'User:EnvlhBot/Reports/{}'.format(dico.get_property_id()))
-    report_page.text = errors_string
-    # report_page.save('Report update for {}'.format(dico.get_edit_summary()))
-    utils.file_put_contents('data/report_errors.txt', errors_string)
-    print('lexemes: {}, success: {}'.format(len(lexemes), success_count))
+    dicos = [LeRobert(db), Littre(db)]
+    for dico in dicos:
+        print('Processing {}...'.format(dico.get_property_id()))
+        lexemes = dico.fetch_lexemes_to_crawl()
+        success_count = 0
+        errors = dict()
+        ref = dict()
+        for lexeme in lexemes:
+            lexeme_id = lexeme['lexeme']['value'][31:]
+            if not db.exists_history(lexeme_id, dico.get_property_id()):
+                success, inferred_id = dico.process(lexeme)
+                if success:
+                    add_value(site, db, lexeme_id, dico.get_property_id(), inferred_id, dico.get_edit_summary())
+                    success_count += 1
+                else:
+                    errors[lexeme_id] = inferred_id
+                    ref[lexeme_id] = lexeme
+        errors = dict(sorted(errors.items(), key=lambda item: item[1]))
+        errors_string = '{| class="wikitable sortable"\n! Lexeme\n! Lemma\n! Lexical category\n! Grammatical gender\n! Inferred ID\n'
+        for lexeme_id in errors:
+            errors_string += '|-\n'
+            errors_string += '| [[Lexeme:' + lexeme_id + '|' + lexeme_id + ']]\n'
+            errors_string += '| ' + ref[lexeme_id]['lemma']['value'] + '\n'
+            errors_string += '| ' + ref[lexeme_id]['lexicalCategoryLabel']['value'] + '\n'
+            errors_string += '| '
+            if 'genderLabel' in ref[lexeme_id]:
+                errors_string += ref[lexeme_id]['genderLabel']['value']
+            errors_string += '\n'
+            errors_string += '| '
+            if errors[lexeme_id] != '':
+                errors_string += '[' + dico.get_url().format(errors[lexeme_id]).replace(' ', '%20') + ' ' + errors[lexeme_id] + ']'
+            errors_string += '\n'
+        errors_string += '|}'
+        report_page = pywikibot.Page(site, 'User:EnvlhBot/Reports/{}'.format(dico.get_property_id()))
+        report_page.text = errors_string
+        # report_page.save('Report update for {}'.format(dico.get_edit_summary()))
+        utils.file_put_contents('data/report_errors.txt', errors_string)
+        print('lexemes: {}, success: {}'.format(len(lexemes), success_count))
 
 
 if __name__ == '__main__':
