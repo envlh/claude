71765877d8c937448c828317e5ef18bbf75ad6c3
Envel Le Hir <envel@lehir.net>
2022-08-15 22:50:20 +0200

    tlfi: wip

--- tlfi.py
+++ tlfi.py
@@ -10,19 +10,39 @@ class Tlfi(Dico):
         return 'P7722'
 
     def get_lexemes_to_crawl_query(self):
-        raise NotImplementedError()
+        return '''SELECT DISTINCT ?lexeme ?lemma ?lexicalCategoryLabel (GROUP_CONCAT(?genderLabel_ ; separator=",") AS ?genderLabel) {
+  # VALUES ?lexeme { wd:L621228 } .
+  ?lexeme dct:language wd:Q150 ; wikibase:lemma ?lemma ; wikibase:lexicalCategory ?lexicalCategory ; schema:dateModified ?dateModified .
+  FILTER NOT EXISTS { ?lexeme wdt:P7722 [] }
+  BIND((NOW() - "P2D"^^xsd:duration) AS ?dateLimit)
+  # FILTER (?dateModified < ?dateLimit) .
+  FILTER (?lexicalCategory != wd:Q162940) . # diacritique
+  FILTER (?lexicalCategory != wd:Q9788) . # lettre
+  FILTER (?lexicalCategory != wd:Q147276) . # nom propre
+  ?lexicalCategory rdfs:label ?lexicalCategoryLabel .
+  FILTER(LANG(?lexicalCategoryLabel) = "fr") .
+  OPTIONAL {
+    ?lexeme wdt:P5185 ?gender .
+    ?gender rdfs:label ?genderLabel_ .
+    FILTER(LANG(?genderLabel_) = "fr")
+  }
+}
+GROUP BY ?lexeme ?lemma ?lexicalCategoryLabel
+LIMIT 10
+'''
 
     def get_url(self):
         return 'https://www.cnrtl.fr/definition/{}'
 
     def infer_id(self, lemma):
-        raise NotImplementedError()
+        return lemma.lower()
 
     def is_matching(self, content, lemma, lexical_category, gender):
-        raise NotImplementedError()
+        inferred_id = self.infer_id(lemma)
+        return 'sendRequest(5,\'/definition/{}//0\')'.format(inferred_id) in content and 'sendRequest(5,\'/definition/{}//1\')'.format(inferred_id) not in content
 
     def get_id_from_redirect(self, r):
         raise NotImplementedError()
 
     def get_edit_summary(self):
-        raise NotImplementedError()
+        return '[[:d:Wikidata:Requests for permissions/Bot/EnvlhBot 3|French dictionaries import]]'
