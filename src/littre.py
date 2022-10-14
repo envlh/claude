ea8ef84f8eeca42c739877a38f89f2104354ef16
Envel Le Hir <envel@lehir.net>
2022-10-14 23:23:51 +0200

    littre

--- src/littre.py
+++ src/littre.py
@@ -3,6 +3,7 @@ import re
 import urllib.parse
 
 from dico import Dico
+from candidate import Candidate
 
 
 class Littre(Dico):
@@ -14,24 +15,18 @@ class Littre(Dico):
         return 'P7724'
 
     def get_lexemes_to_crawl_query(self):
-        return '''SELECT DISTINCT ?lexeme ?lemma ?lexicalCategoryLabel (GROUP_CONCAT(?genderLabel_ ; separator=",") AS ?genderLabel) {
+        return '''SELECT DISTINCT ?lexeme ?lemma ?lexicalCategory (GROUP_CONCAT(?gender ; separator=",") AS ?genders) {
   # VALUES ?lexeme { wd:L621228 } .
   ?lexeme dct:language wd:Q150 ; wikibase:lemma ?lemma ; wikibase:lexicalCategory ?lexicalCategory ; schema:dateModified ?dateModified .
   FILTER NOT EXISTS { ?lexeme wdt:P7724 [] }
-  BIND((NOW() - "P2D"^^xsd:duration) AS ?dateLimit)
+  # BIND((NOW() - "P2D"^^xsd:duration) AS ?dateLimit)
   # FILTER (?dateModified < ?dateLimit) .
   FILTER (?lexicalCategory != wd:Q162940) . # diacritique
   FILTER (?lexicalCategory != wd:Q9788) . # lettre
   FILTER (?lexicalCategory != wd:Q147276) . # nom propre
-  ?lexicalCategory rdfs:label ?lexicalCategoryLabel .
-  FILTER(LANG(?lexicalCategoryLabel) = "fr") .
-  OPTIONAL {
-    ?lexeme wdt:P5185 ?gender .
-    ?gender rdfs:label ?genderLabel_ .
-    FILTER(LANG(?genderLabel_) = "fr")
-  }
+  OPTIONAL { ?lexeme wdt:P5185 ?gender }
 }
-GROUP BY ?lexeme ?lemma ?lexicalCategoryLabel
+GROUP BY ?lexeme ?lemma ?lexicalCategory
 LIMIT 100000
 '''
 
@@ -41,7 +36,8 @@ LIMIT 100000
     def infer_id(self, lemma):
         return lemma.replace('œ', 'oe')
 
-    def is_matching(self, content, lemma, lexical_category, gender):
+    def parse_content(self, content, inferred_id):
+        candidates = set()
         match = re.search(re.compile('<section class="definition"><h2(?: class="[a-z]*")?>(.*?)</h2>.*?<div class="entete">.*?<b>(.*?)</b>', re.DOTALL), content)
         if match is not None:
             lem_match = match.group(1).strip()
@@ -50,26 +46,36 @@ LIMIT 100000
                 first_char = lemmas_match[i][:1]
                 position = lemmas_match[0].rfind(first_char)
                 lemmas_match[i] = lemmas_match[0][:position] + lemmas_match[i]
-            lexcat_match = match.group(2)
-            if lemma in lemmas_match:
-                if lexical_category == 'nom':
-                    if gender == 'féminin' and lexcat_match == '<abbr title="substantif féminin">s. f.</abbr>':
-                        return True
-                    if gender == 'masculin' and lexcat_match == '<abbr title="substantif masculin">s. m.</abbr>':
-                        return True
-                    if gender in ('féminin,masculin', 'masculin,féminin') and lexcat_match == '<abbr title="substantif masculin et féminin">s. m. et f.</abbr>':
-                        return True
-                    if len(lemmas_match) == 2 and gender in ('féminin', 'masculin') and lexcat_match == '<abbr title="substantif masculin et féminin">s. m. et f.</abbr>':
-                        return True
-                if lexical_category == 'verbe' and lexcat_match in ('<abbr title="verbe neutre, notion grammaticale désuete">v. n.</abbr>', '<abbr title="verbe transitif, anciennement appelé verbe actif">v. a.</abbr>'):
-                    return True
-                if lexical_category == 'adjectif' and lexcat_match == '<abbr title="adjectif">adj.</abbr>':
-                    return True
-                if lexical_category == 'adverbe' and lexcat_match == '<abbr title="adverbe">adv.</abbr>':
-                    return True
-            # print(lemma, lexical_category, gender, lem_match, lexcat_match)
-        # print(lemma, lexical_category, gender, match)
-        return False
+            matched_lexical_category = match.group(2)
+            lexical_categories = set()
+            genders = set()
+            if matched_lexical_category in ('<abbr title="substantif féminin">s. f.</abbr>', '<abbr title="substantif féminin">S. f.</abbr>', '<abbr title="substantif féminin pluriel">s. f. pl.</abbr>'):
+                lexical_categories.add(self.NOUN)
+                genders.add(self.FEMININE)
+            elif matched_lexical_category in ('<abbr title="substantif masculin">s. m.</abbr>', '<abbr title="substantif masculin">S. m.</abbr>', '<abbr title="substantif masculin pluriel">s. m. plur.</abbr>', '<abbr title="substantif masculin pluriel">s. m. pl.</abbr>'):
+                lexical_categories.add(self.NOUN)
+                genders.add(self.MASCULINE)
+            elif matched_lexical_category == '<abbr title="substantif masculin et féminin">s. m. et f.</abbr>':
+                lexical_categories.add(self.NOUN)
+                genders.add(self.FEMININE)
+                genders.add(self.MASCULINE)
+            elif matched_lexical_category in ('<abbr title="verbe neutre, notion grammaticale désuete">v. n.</abbr>', '<abbr title="verbe neutre, notion grammaticale désuete">V. n.</abbr>', '<abbr title="verbe transitif, anciennement appelé verbe actif">v. a.</abbr>', '<abbr title="verbe transitif, anciennement appelé verbe actif">V. a.</abbr>', '<abbr title="verbe pronominal, anciennement appelé verbe réflexif">v. réfl.</abbr>'):
+                lexical_categories.add(self.VERB)
+            elif matched_lexical_category in ('<abbr title="adjectif">adj.</abbr>', '<abbr title="adjectif">adj.</abbr>', '<abbr title="adjectif féminin">adj. f.</abbr>', '<abbr title="adjectif masculin">adj. m.</abbr>'):
+                lexical_categories.add(self.ADJECTIVE)
+            elif matched_lexical_category == '<abbr title="adverbe">adv.</abbr>':
+                lexical_categories.add(self.ADVERB)
+            elif matched_lexical_category == '<abbr title="locution adverbiale">loc. adv.</abbr>':
+                lexical_categories.add(self.LOCUTION_ADVERBIAL)
+            else:
+                if matched_lexical_category in self.unknown_lexical_categories:
+                    self.unknown_lexical_categories[matched_lexical_category] += 1
+                else:
+                    self.unknown_lexical_categories[matched_lexical_category] = 1
+            for lemma in lemmas_match:
+                for lexical_category in lexical_categories:
+                    candidates.add(Candidate(inferred_id, lemma, lexical_category, genders))
+        return candidates
 
     def get_id_from_redirect(self, r):
         redirect_id = urllib.parse.unquote(json.loads(r['headers'])['Location'][12:])
